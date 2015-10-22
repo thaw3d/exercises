3569d22f64ca3301c8690ab14d80c8ec608ce169
thaw3d <thaw3dex@gmail.com>
2015-10-22 13:27:31 +0200

    added BufferedWriter

--- writeFile.java
+++ writeFile.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class writeFile {
   public static void main(String[] arg) throws IOException {
-   PrintWriter pw = new PrintWriter (new FileWriter("file.txt"));
+   PrintWriter pw = new PrintWriter (new BufferedWriter(new FileWriter("file.txt")));
    Scanner sc = new Scanner (System.in);
    System.out.println("Write 10 words.");
       for (int i=0; i<=10; i++) {
