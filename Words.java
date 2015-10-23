dbdfa8bd47be7039a13628ada3ccd960484eab06
thaw3d <thaw3dex@gmail.com>
2015-10-23 22:16:39 +0200

    contains() before substring

---
+++ Words.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Words {
+  public static void main(String[] arg) throws IOException {
+      BufferedReader instrom = new BufferedReader
+       (new InputStreamReader(System.in));
+   PrintWriter utstrom = new PrintWriter
+                    (new BufferedWriter
+                    (new FileWriter("filen.txt")));
+    
+    System.out.println("Skriv 10 ord.");
+
+    int ord = 0;
+    while (ord <10) {
+      
+      String namn = instrom.readLine();
+      
+        if (namn.contains(" ")) {
+        int i = namn.indexOf(" ");
+        utstrom.println(namn.substring(0,i));
+        ord++;
+      }
+        else {
+        utstrom.println(namn);
+        ord++;
+      }
+  }
+
+    utstrom.close();
+  }      
+}
