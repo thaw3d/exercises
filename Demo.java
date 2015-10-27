fe43552ee46cceaa83c283ac2fe2e4cea219e67b
thaw3d <thaw3dex@gmail.com>
2015-10-27 15:16:34 +0100

    idk

---
+++ Demo.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Demo {
+	
+	public static void main (String[] args) {
+
+	    String s = "s";
+
+	    System.out.println("längden på input är:" + s.length());
+	    System.out.println("Bokstaven på index 0 är: " + s.charAt(0));
+     
+
+  int[] tal = new int [100];
+
+
+   for (int i = 0; tal.length; i++) {
+     tal[i] = 100 - i;
+
+	}
+
+	 for (int i = 0; tal.length; i++) {
+     tal[i] = 100 - i;
+
+	}
+
+	
+	   System.out.println(Arrays.toString(tal));
+}
+}
+
+
+class Hello {
+
+  
+} 
